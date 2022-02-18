95621f31e04f7d0e0e03fd170d041421b3c51225
Paris Amerikanos <pamerikanos@outlook.com>
2022-02-18 23:21:21 +0200

    Added capture script

---
+++ rocket_metrics/capture.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from picamera import PiCamera
+from time import sleep
+import datetime
+import os
+
+camera = PiCamera()
+camera.start_preview()
+sleep(5)
+camera.capture(os.path.join(os.path.expanduser('~'), 
+                            'capture', 
+                            datetime.datetime.now().strftime("%Y%m%d%H%M%S"), 
+                            '.jpg'))
+camera.stop_preview()
