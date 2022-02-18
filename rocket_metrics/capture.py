0cd8318b13369bd7f438c875277193982e3435c9
Paris Amerikanos <pamerikanos@outlook.com>
2022-02-18 23:25:58 +0200

    Fixes

--- rocket_metrics/capture.py
+++ rocket_metrics/capture.py
@@ -9,9 +9,7 @@ camera = PiCamera()
 camera.start_preview()
 print("Please wait...")
 sleep(5)
-camera.capture(os.path.join(os.path.expanduser('~'), 
-                            'capture', 
-                            datetime.datetime.now().strftime("%Y%m%d%H%M%S"), 
-                            '.jpg'))      
+camera.capture(os.path.join(os.path.expanduser('~'), 'capture', 
+                            datetime.datetime.now().strftime("%Y%m%d%H%M%S") + '.jpg'))      
 camera.stop_preview()
 print("Image captured successfully")
