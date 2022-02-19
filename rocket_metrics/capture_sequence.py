d07f5e961a784c08744d060bb07f97ae75b418eb
Paris Amerikanos <pamerikanos@outlook.com>
2022-02-19 22:43:31 +0200

    seq

--- rocket_metrics/capture_sequence.py
+++ rocket_metrics/capture_sequence.py
@@ -1,70 +1,23 @@
-import io
 import time
-import threading
 import picamera
 
-# Create a pool of image processors
-done = False
-lock = threading.Lock()
-pool = []
+frames = 60
 
-class ImageProcessor(threading.Thread):
-    def __init__(self):
-        super(ImageProcessor, self).__init__()
-        self.stream = io.BytesIO()
-        self.event = threading.Event()
-        self.terminated = False
-        self.start()
-
-    def run(self):
-        # This method runs in a separate thread
-        global done
-        while not self.terminated:
-            # Wait for an image to be written to the stream
-            if self.event.wait(1):
-                try:
-                    self.stream.seek(0)
-                    # Read the image and do some processing on it
-                    #Image.open(self.stream)
-                    #...
-                    #...
-                    # Set done to True if you want the script to terminate
-                    # at some point
-                    #done=True
-                finally:
-                    # Reset the stream and event
-                    self.stream.seek(0)
-                    self.stream.truncate()
-                    self.event.clear()
-                    # Return ourselves to the pool
-                    with lock:
-                        pool.append(self)
-
-def streams():
-    while not done:
-        with lock:
-            if pool:
-                processor = pool.pop()
-            else:
-                processor = None
-        if processor:
-            yield processor.stream
-            processor.event.set()
-        else:
-            # When the pool is starved, wait a while for it to refill
-            time.sleep(0.1)
+def filenames():
+    frame = 0
+    while frame < frames:
+        yield 'image%02d.jpg' % frame
+        frame += 1
 
 with picamera.PiCamera() as camera:
-    pool = [ImageProcessor() for i in range(4)]
-    camera.resolution = (640, 480)
+    camera.resolution = (1024, 768)
     camera.framerate = 30
     camera.start_preview()
+    # Give the camera some warm-up time
     time.sleep(2)
-    camera.capture_sequence(streams(), use_video_port=True)
-
-# Shut down the processors in an orderly fashion
-while pool:
-    with lock:
-        processor = pool.pop()
-    processor.terminated = True
-    processor.join()
+    start = time.time()
+    camera.capture_sequence(filenames(), use_video_port=True)
+    finish = time.time()
+print('Captured %d frames at %.2ffps' % (
+    frames,
+    frames / (finish - start)))
