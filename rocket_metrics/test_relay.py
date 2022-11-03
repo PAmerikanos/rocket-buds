787fae76192ea40f4bd3baddb7dc30209e8775bb
Paris Amerikanos <pamerikanos@outlook.com>
2022-11-03 10:10:23 +0200

    Simplified relay test script

--- rocket_metrics/test_relay.py
+++ rocket_metrics/test_relay.py
@@ -1,41 +1,15 @@
-# getting the main GPIO libraly
 import RPi.GPIO as GPIO
-# getting the time libraly
 import time
 
-# setting a current mode
+SPARK_DURATION = 2
+CHARGE_PIN = 23
+
 GPIO.setmode(GPIO.BCM)
-#removing the warings 
 GPIO.setwarnings(False)
-#creating a list (array) with the number of GPIO's that we use 
-pins = [23]
+GPIO.setup(CHARGE_PIN, GPIO.OUT)
 
-#setting the mode for all pins so all will be switched on 
-GPIO.setup(pins, GPIO.OUT)
+GPIO.output(CHARGE_PIN,  GPIO.HIGH)
+time.sleep(SPARK_DURATION)
+GPIO.output(CHARGE_PIN,  GPIO.LOW)
 
-#for loop where pin = 18 next 17 ,15, 14 
-for pin in pins :
-	#setting the GPIO to HIGH or 1 or true
-	GPIO.output(pin,  GPIO.HIGH)
-	#wait 0,5 second
-	time.sleep(2)
-	#setting the GPIO to LOW or 0 or false
-	GPIO.output(pin,  GPIO.LOW)
-	#wait 0,5 second
-	time.sleep(2)
-	#setting the GPIO to HIGH or 1 or true
-	GPIO.output(pin,  GPIO.HIGH)
-	#wait 0,5 second
-	time.sleep(2)
-	#setting the GPIO to LOW or 0 or false
-	GPIO.output(pin,  GPIO.LOW)
-	#wait 0,5 second
-	time.sleep(2)
-
-	#Checking if the current relay is running and printing it 
-	if not GPIO.input(pin):
-		print("Pin ", str(pin), " is working")
-
-#cleaning all GPIO's 
 GPIO.cleanup()
-print("Shutdown All relays")
