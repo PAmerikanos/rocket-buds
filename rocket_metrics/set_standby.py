958d4449f9439aad1c542dad622fd125569de548
Paris Amerikanos <pamerikanos@outlook.com>
2023-04-18 11:33:54 +0300

    Added standby as service

---
+++ rocket_metrics/set_standby.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+import RPi.GPIO as GPIO
+
+channel = 26
+LED_UP_PIN = 16
+flag = False
+
+def button_callback(channel):
+    print("Button was pushed!")
+    global flag
+    if flag:
+        GPIO.output(LED_UP_PIN, GPIO.LOW)
+        flag = False
+    else:
+        GPIO.output(LED_UP_PIN, GPIO.HIGH)
+        flag = True
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(channel, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(LED_UP_PIN, GPIO.OUT)
+
+GPIO.add_event_detect(channel,GPIO.RISING,callback=button_callback,bouncetime=1000)
+
+while True:
+    pass
+#message = input("Do sth")
+
+GPIO.cleanup()
