cd727e7da3cd1c4d15facf751bd2f14803ed56f6
Paris Amerikanos <pam@pam.gr>
2025-12-27 18:11:08 +0200

    fix: indentation

--- rocket_metrics/capture_data_deploy_parachute.py
+++ rocket_metrics/capture_data_deploy_parachute.py
@@ -109,99 +109,99 @@ if __name__ == '__main__':
     try:
         camera.start_preview()
 
-            # Open data file and add header
-            with open(os.path.join(sensor_dir, get_curr_time() + ".csv"), mode="w") as file:
-                file.write('time_curr,accel_x,accel_y,accel_z,gyro_x,gyro_y,gyro_z,temp_c,pres_pa,alt_m,ignition_status\n')
+        # Open data file and add header
+        with open(os.path.join(sensor_dir, get_curr_time() + ".csv"), mode="w") as file:
+            file.write('time_curr,accel_x,accel_y,accel_z,gyro_x,gyro_y,gyro_z,temp_c,pres_pa,alt_m,ignition_status\n')
 
-                previous_1_alt_m = ground_alt_m
-                previous_2_alt_m = ground_alt_m
-                previous_3_alt_m = ground_alt_m
+            previous_1_alt_m = ground_alt_m
+            previous_2_alt_m = ground_alt_m
+            previous_3_alt_m = ground_alt_m
 
-                start_run_time = time.time()
-                ignition_status = ""
+            start_run_time = time.time()
+            ignition_status = ""
 
-                #while True:
-                while LOOP_FLAG:
-                    if PREVIOUS_FLAG == "Standby":
-                        GPIO.output(LED_UP_PIN, GPIO.HIGH)
-                        GPIO.output(LED_DOWN_PIN, GPIO.LOW)
-                        time.sleep(0.5)
+            #while True:
+            while LOOP_FLAG:
+                if PREVIOUS_FLAG == "Standby":
+                    GPIO.output(LED_UP_PIN, GPIO.HIGH)
+                    GPIO.output(LED_DOWN_PIN, GPIO.LOW)
+                    time.sleep(0.5)
+                    GPIO.output(LED_UP_PIN, GPIO.LOW)
+                    GPIO.output(LED_DOWN_PIN, GPIO.HIGH)
+                    time.sleep(0.5)
+                elif PREVIOUS_FLAG == "Record":
+                    # Gather data at 2 Hz
+                    time.sleep(0.5)
+
+                    accel_data = mpu6050_sensor.get_accel_data()
+                    gyro_data = mpu6050_sensor.get_gyro_data()
+                    temperature, pressure, altitude = bmp388_sensor.get_temperature_and_pressure_and_altitude()
+
+                    time_curr = get_curr_time()
+                    accel_x = accel_data['x']
+                    accel_y = accel_data['y']
+                    accel_z = accel_data['z']
+                    gyro_x = gyro_data['x']
+                    gyro_y = gyro_data['y']
+                    gyro_z = gyro_data['z']
+                    temp_c = temperature / 100.0
+                    pres_pa = pressure / 100.0
+                    current_alt_m = altitude / 100.0 - ground_alt_m
+
+                    # Write photos to file
+                    img_path = os.path.join(capture_dir, time_curr + '.jpg')
+                    camera.capture(img_path, format='jpeg', use_video_port=False, resize=None, quality=85, thumbnail=None, bayer=False)
+
+                    attitude = "UP" if current_alt_m > previous_1_alt_m else "DOWN"
+                    print(f'{time_curr}: RECORDING & CAPTURING @{current_alt_m}m - {attitude}')
+
+                    # Arm CHARGE & LEDs when at least 10m above ground
+                    if MINIMUM_SAFE_HEIGHT_m + MEASUREMENT_ERROR_m < current_alt_m:
+                        print("ARMED: Above minimum safe height")
+
+                        # Illuminate LEDs according to rocket attitude
+                        if current_alt_m > previous_1_alt_m:
+                            GPIO.output(LED_UP_PIN, GPIO.HIGH)
+                            GPIO.output(LED_DOWN_PIN, GPIO.LOW)
+                        else:
+                            GPIO.output(LED_UP_PIN, GPIO.LOW)
+                            GPIO.output(LED_DOWN_PIN, GPIO.HIGH)
+
+                        # Activate charge if altitude is not increasing for 3 measurements and spark has not been lit before
+                        if (current_alt_m < previous_1_alt_m) and (current_alt_m < previous_2_alt_m) and (current_alt_m < previous_3_alt_m) and ignition_status == "":
+                            time.sleep(CHARGE_DELAY_s)
+                            GPIO.output(CHARGE_PIN,  GPIO.HIGH)
+                            start_spark_time = time.time()
+                            print("IGNITION: Start")
+                            ignition_status = "Start"
+                            END_SPARK = True
+                    else: # Unarm LEDs if below min safe height
                         GPIO.output(LED_UP_PIN, GPIO.LOW)
-                        GPIO.output(LED_DOWN_PIN, GPIO.HIGH)
-                        time.sleep(0.5)
-                    elif PREVIOUS_FLAG == "Record":
-                        # Gather data at 2 Hz
-                        time.sleep(0.5)
+                        GPIO.output(LED_DOWN_PIN, GPIO.LOW)
 
-                        accel_data = mpu6050_sensor.get_accel_data()
-                        gyro_data = mpu6050_sensor.get_gyro_data()
-                        temperature, pressure, altitude = bmp388_sensor.get_temperature_and_pressure_and_altitude()
+                    # Stop spark after 2 seconds
+                    if END_SPARK:
+                        if time.time() - start_spark_time >= SPARK_DURATION_s:
+                            GPIO.output(CHARGE_PIN,  GPIO.LOW)
+                            print("IGNITION: Stop")
+                            ignition_status = "Stop"
+                            END_SPARK = False
 
-                        time_curr = get_curr_time()
-                        accel_x = accel_data['x']
-                        accel_y = accel_data['y']
-                        accel_z = accel_data['z']
-                        gyro_x = gyro_data['x']
-                        gyro_y = gyro_data['y']
-                        gyro_z = gyro_data['z']
-                        temp_c = temperature / 100.0
-                        pres_pa = pressure / 100.0
-                        current_alt_m = altitude / 100.0 - ground_alt_m
+                    # Write measurements to file
+                    measurement_str = f'{time_curr},{accel_x},{accel_y},{accel_z},{gyro_x},{gyro_y},{gyro_z},{temp_c},{pres_pa},{current_alt_m},{ignition_status}\n'
+                    file.write(measurement_str)
 
-                        # Write photos to file
-                        img_path = os.path.join(capture_dir, time_curr + '.jpg')
-                        camera.capture(img_path, format='jpeg', use_video_port=False, resize=None, quality=85, thumbnail=None, bayer=False)
+                    previous_3_alt_m = previous_2_alt_m
+                    previous_2_alt_m = previous_1_alt_m
+                    previous_1_alt_m = current_alt_m
 
-                        attitude = "UP" if current_alt_m > previous_1_alt_m else "DOWN"
-                        print(f'{time_curr}: RECORDING & CAPTURING @{current_alt_m}m - {attitude}')
-
-                        # Arm CHARGE & LEDs when at least 10m above ground
-                        if MINIMUM_SAFE_HEIGHT_m + MEASUREMENT_ERROR_m < current_alt_m:
-                            print("ARMED: Above minimum safe height")
-
-                            # Illuminate LEDs according to rocket attitude
-                            if current_alt_m > previous_1_alt_m:
-                                GPIO.output(LED_UP_PIN, GPIO.HIGH)
-                                GPIO.output(LED_DOWN_PIN, GPIO.LOW)
-                            else:
-                                GPIO.output(LED_UP_PIN, GPIO.LOW)
-                                GPIO.output(LED_DOWN_PIN, GPIO.HIGH)
-
-                            # Activate charge if altitude is not increasing for 3 measurements and spark has not been lit before
-                            if (current_alt_m < previous_1_alt_m) and (current_alt_m < previous_2_alt_m) and (current_alt_m < previous_3_alt_m) and ignition_status == "":
-                                time.sleep(CHARGE_DELAY_s)
-                                GPIO.output(CHARGE_PIN,  GPIO.HIGH)
-                                start_spark_time = time.time()
-                                print("IGNITION: Start")
-                                ignition_status = "Start"
-                                END_SPARK = True
-                        else: # Unarm LEDs if below min safe height
-                                GPIO.output(LED_UP_PIN, GPIO.LOW)
-                                GPIO.output(LED_DOWN_PIN, GPIO.LOW)
-
-                        # Stop spark after 2 seconds
-                        if END_SPARK:
-                            if time.time() - start_spark_time >= SPARK_DURATION_s:
-                                GPIO.output(CHARGE_PIN,  GPIO.LOW)
-                                print("IGNITION: Stop")
-                                ignition_status = "Stop"
-                                END_SPARK = False
-
-                        # Write measurements to file
-                        measurement_str = f'{time_curr},{accel_x},{accel_y},{accel_z},{gyro_x},{gyro_y},{gyro_z},{temp_c},{pres_pa},{current_alt_m},{ignition_status}\n'
-                        file.write(measurement_str)
-
-                        previous_3_alt_m = previous_2_alt_m
-                        previous_2_alt_m = previous_1_alt_m
-                        previous_1_alt_m = current_alt_m
-
-                        #if time.time() - start_run_time >= AUTO_SHUTDOWN_s:
-                        #if TERMINATE_PROCESS:
-                            # GPIO.output(LED_UP_PIN, GPIO.LOW)
-                            # GPIO.output(LED_DOWN_PIN, GPIO.LOW)
-                            # GPIO.cleanup()
-                            # camera.stop_preview()
-                            # sys.exit()
+                    #if time.time() - start_run_time >= AUTO_SHUTDOWN_s:
+                    #if TERMINATE_PROCESS:
+                        # GPIO.output(LED_UP_PIN, GPIO.LOW)
+                        # GPIO.output(LED_DOWN_PIN, GPIO.LOW)
+                        # GPIO.cleanup()
+                        # camera.stop_preview()
+                        # sys.exit()
                         
     except KeyboardInterrupt:
         print("\nScript interrupted by user")
